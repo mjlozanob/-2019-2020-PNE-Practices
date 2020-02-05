41fed0f66f6a54a381d0ab1dbb40b0ed2c96e9d3
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-02-05 17:44:21 +0100

    Session 2

--- Session-02/ex2.py
+++ Session-02/ex2.py
@@ -1,9 +1,9 @@
 for i in range(12):
     if i == 0:
-        print(0)
+        print(i)
     elif i > 0:
-        n1 = i -1
-        n2 = i
+        n1 = 0
+        n2 = 1
         n3 = n1 + n2
         print(n3)
         n1 = n2
