e5f83cbba827611681eb2fa99e74b9054ce43cbe
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-02-11 16:30:28 +0100

    Session 4

---
+++ Session-04/head.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "RNU6_269P.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+print(file_contents.split("\n"))
+
