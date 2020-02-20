e2ac407d628a007d923c1d16609ff32f51d5d386
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-02-20 11:38:41 +0100

    Practice 0

---
+++ P0/Ex8.py
@@ -0,0 +1,14 @@
+from Seq0 import *
+
+Genes = ["ADA", "FRAT1", "FXN", "U5", "RNU6_269P"]
+Folder = "../Session-04/"
+end = ".txt"
+Bases = ["A", "C", "G", "T"]
+base_list = []
+
+for gene in Genes:
+    for element in Bases:
+        filename = Folder + gene + end
+        sequence = seq_read_fasta(filename)
+        data = seq_count(sequence)
+    print("Gene", gene, ": Most frequent Base: ",max(data, key=data.get))
