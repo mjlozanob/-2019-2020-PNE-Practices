8031371bc54f043f746bd5592ac63efada8be866
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-04-19 19:10:37 +0200

    Session 06

--- Session-06/Ex4.py
+++ Session-06/Ex4.py
@@ -1,3 +1,5 @@
+import termcolor
+
 class Seq:
     """A class for representing sequences"""
     def __init__(self, strbases):
@@ -31,12 +33,13 @@ def generate_seqs(pattern, number):
     return sequence
 
 # Main program
+
 seq_list1 = generate_seqs("A", 3)
 seq_list2 = generate_seqs("AC", 5)
 
-print("List 1:")
-print_seqs(seq_list1)
+termcolor.cprint("List 1:", "blue")
+termcolor.cprint(print_seqs(seq_list1), "blue")
 
 
-print("List 2:")
-print_seqs(seq_list2)
+termcolor.cprint("List 2:", "green")
+termcolor.cprint((print_seqs(seq_list2)), "green")
