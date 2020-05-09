df861b753659112218270692f0f4cb6bd8e5fc6b
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-05-09 20:52:25 +0200

    Session 18 done

--- Session-18/test-json-EX01.py
+++ Session-18/test-json-EX01.py
@@ -12,25 +12,27 @@ person = json.loads(jsonstring)
 # associated to the fields 'Firstname', 'Lastname' and 'age'
 
 # Print the information on the console, in colors
-print()
-termcolor.cprint("Name: ", 'green', end="")
-print(person['Firstname'], person['Lastname'])
-termcolor.cprint("Age: ", 'green', end="")
-print(person['age'])
+print("Total people in the database:", len(person))
+for e in person:
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(e['Firstname'], e['Lastname'])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(e['age'])
 
 # Get the phoneNumber list
-phoneNumbers = person['phoneNumber']
+    phoneNumbers = e['phoneNumber']
 
 # Print the number of elements in the list
-termcolor.cprint("Phone numbers: ", 'green', end='')
-print(len(phoneNumbers))
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
 
 # Print all the numbers
-for i, num in enumerate(phoneNumbers):
-    termcolor.cprint("  Phone {}:".format(i), 'blue')
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
 
     # The element num contains 2 fields: number and type
-    termcolor.cprint("    Type: ", 'red', end='')
-    print(num['type'])
-    termcolor.cprint("    Number: ", 'red', end='')
-    print(num['number'])
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
