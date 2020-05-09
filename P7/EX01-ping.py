490989f6c84b307c2b58d51895339ee9482903a6
mjlozanob <mj.lozano.2019@alumnos.urjc.es>
2020-05-09 23:41:55 +0200

    Ex 1 done

---
+++ P7/EX01-ping.py
@@ -0,0 +1,38 @@
+import http.client
+import json
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+PARAMS = '?content-type=application/json'
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", ENDPOINT+PARAMS)
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+ping = response['ping']
+if ping == 1:
+    print('PING OK! The database is running')
+
+# -- Print the received data
+print(f"CONTENT: {data1}")
